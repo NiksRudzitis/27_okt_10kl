d67f9fab45c54edea88271df13bbc91f07a78a24
NiksRudzitis <niksrudz@madona.edu.lv>
2025-10-27 11:47:48 +0200

    Izlabotas kļūdas uzd4.py

--- uzd4.py
+++ uzd4.py
@@ -4,7 +4,8 @@ summma=0
 skaitlis=1
 
 while skaitlis<=100:
-    summma+skaitlis # summma=summma+skaitlis
-    skaitlis+1
+    summma+=skaitlis # summma=summma+skaitlis
+    skaitlis+=1
 
 print("Summa no 1 līdz 100 ir:summma}")
+
